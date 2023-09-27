24f4af0841a454fe6c6c89bebcf2ee419970f359
Влад Козлов <vkozlov15012005@gmail.com>
2023-09-27 19:01:39 +0300

    Добавьте файлы проекта.

---
+++ Body-count calculator/Body-count calculator/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Body_count_calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            double height = Convert.ToDouble(heightEn.Text) / 100;
+            double weight = Convert.ToDouble(weightEn.Text);
+            double index = weight / Math.Pow(2, height);
+            string result = string.Format("{0:f}", index);
+            if (index < 18.5)
+            {
+                imt.Text = result + "    Недостаток веса";
+                imt.TextColor = Color.Blue;
+            }
+            else if (index >= 18.5 & index <= 25)
+            {
+                imt.Text = result + "    Норма";
+                imt.TextColor = Color.Green;
+            }
+            else if (index > 25)
+            {
+                imt.Text = result + "    Переизбыток веса";
+                imt.TextColor = Color.Red;
+            }
+
+            imt.IsVisible = true;
+
+        }
+    }
+}
