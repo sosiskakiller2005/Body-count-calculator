e3b78812493e603a525577b2183aaa6732067738
Влад Козлов <vkozlov15012005@gmail.com>
2023-09-29 18:52:29 +0300

    Добавил microcharts для визуального сравнения ИМТ. Пока решаю, как сделать так, чтобы щкала появлялась в порядке, зависимом от ИМТ, а не в порядке написания кода

--- Body-count calculator/Body-count calculator/MainPage.xaml.cs
+++ Body-count calculator/Body-count calculator/MainPage.xaml.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Microcharts;
+using Entry = Microcharts.ChartEntry;
+using SkiaSharp;
+using System.IO;
+
 
 namespace Body_count_calculator
 {
@@ -12,7 +17,7 @@ namespace Body_count_calculator
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();  
         }
 
         private void OnButtonClicked(object sender, System.EventArgs e)
@@ -37,6 +42,43 @@ namespace Body_count_calculator
                 imt.TextColor = Color.Red;
             }
 
+            List<Entry> entries = new List<Entry>
+            {
+                new Entry(18.5f)
+                {
+                    Color = SKColor.Parse("#6369D1"),
+                    Label = "Недостаток веса",
+                    ValueLabel="18,5",
+                    ValueLabelColor = SKColor.Parse("#6369D1"),
+                },
+                new Entry(25f)
+                {
+                    Color = SKColor.Parse("#AAFAC8"),
+                    Label = "Норма",
+                    ValueLabel="19-25",
+                    ValueLabelColor = SKColor.Parse("#AAFAC8"),
+                },
+                new Entry(30f)
+                {
+                    Color = SKColor.Parse("#EF798A"),
+                    Label = "Переизбыток веса",
+                    ValueLabelColor = SKColor.Parse("#EF798A"),
+                    ValueLabel="30",
+                },
+                new Entry(Convert.ToSingle(index))
+                {
+                    Color = SKColor.Parse("#6369D1"),
+                    Label = "Ваш индекс массы тела",
+                    ValueLabel = String.Format("{0:f1}", index),
+                    ValueLabelColor = SKColor.Parse("#6369D1"),
+                    
+                }
+            };
+            imtChart.Chart = new BarChart { Entries = entries, LabelOrientation = Orientation.Horizontal, 
+                ValueLabelOrientation = Orientation.Horizontal, Margin = 15,
+                LabelTextSize = 25};
+
+            imtChart.IsVisible = true;
             imt.IsVisible = true;
 
         }
