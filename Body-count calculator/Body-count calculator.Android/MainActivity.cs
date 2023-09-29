d69df32a1e851ba4737b4d23b74c8d48e144f86c
Влад Козлов <vkozlov15012005@gmail.com>
2023-09-29 20:20:37 +0300

    Добавил иконку, полностью собрал приложение

--- Body-count calculator/Body-count calculator.Android/MainActivity.cs
+++ Body-count calculator/Body-count calculator.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Body_count_calculator.Droid
 {
-    [Activity(Label = "Body_count_calculator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Body_count_calculator", Icon = "@drawable/calculator", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
